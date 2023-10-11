88cac841c8bfa3abdef6536a233f4f7ac399f6c8
Juanma <77707900+JuanmaGuariste@users.noreply.github.com>
2023-10-11 11:11:12 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ if (cluster.isPrimary) {
             
 
             // for (let i = 0; i < cpuCount; i++) {
-            for (let i = 0; i < 8; i++) {
+            for (let i = 0; i < 7; i++) {
                 cluster.fork();
                 logger.info(`CPU fork number${i}`);
 
