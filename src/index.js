d4401c3a3c44701241bab9e0956e8e05de685b06
Juanma <77707900+JuanmaGuariste@users.noreply.github.com>
2023-10-11 10:58:22 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -31,7 +31,8 @@ if (cluster.isPrimary) {
             logger.info(`CPU count: ---- ${cpuCount}`);
             
 
-            for (let i = 0; i < cpuCount; i++) {
+            // for (let i = 0; i < cpuCount; i++) {
+            for (let i = 0; i < 3; i++) {
                 cluster.fork();
                 logger.info(`CPU fork number${i}`);
 
@@ -42,10 +43,12 @@ if (cluster.isPrimary) {
             });
         })
 } else {
+    conbsole.log("ESTOY en el ELSE")
     logger.info(`Worker ${process.pid} started`);
     io = await socketio.socketio();
     io.adapter(createAdapter());
     setupWorker(io);
+    conbsole.log("ESTOY en el ELSE mas abajo")
     emitter.on("new-product", async (product) => {
         let totalProducts = [];
         try {
